f856e17078c0ca99babc09a3696d8c25399bd852
joaobr200 <joaoarantesjob@gmail.com>
2022-07-14 09:32:40 -0300

    loading

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,24 @@ import React from "react";
 import Head from "next/head";
 import { SearchContext } from "../context/SearchContext";
 
-import Home from "../components/home";
-import Dashboard from "../components/dashboard";
+import Home from "../components/Home";
+import Dashboard from "../components/Dashboard";
+import ErrorPopup from "../components/ErrorPopup";
+import DashboardSkeleton from "../components/Skeletons/DashboardSkeleton";
 
 const Landing: React.FC = () => {
-  const { searchState } = React.useContext(SearchContext);
+  const {
+    searchState: { data, error, loading }
+  } = React.useContext(SearchContext);
   return (
     <>
       <Head>
         <title>Github Search</title>
       </Head>
-      {searchState.data ? <Dashboard /> : <Home />}
+      <main>
+        {data ? <Dashboard /> : loading ? <DashboardSkeleton /> : <Home />}
+        {error && <ErrorPopup />}
+      </main>
     </>
   );
 };
